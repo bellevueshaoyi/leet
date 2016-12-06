8d3ec627794c623ba85c8e6e6068496e152b7d89
stevensha <stevensha@gmail.com>
2016-12-06 00:34:47 -0500

    Revert "Update the code."
    This reverts commit 309d8dd25df924271a14c989fbec7740438e9d62.

--- yang-87-scramble-string.java
+++ yang-87-scramble-string.java
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/scramble-string/.
 public class Solution {
     public boolean isScramble(String s1, String s2) {
-        return true;
+        return false;
     }
 }
