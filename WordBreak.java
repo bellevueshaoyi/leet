e1ff6bd2cd596dd4d05842e89a280eff7b52c5bf
bellevueshaoyi <tiankong82@live.com>
2017-01-12 23:14:56 -0800

    Update WordBreak.java

--- WordBreak.java
+++ WordBreak.java
@@ -20,3 +20,32 @@ public class Solution {
         return false;
     }
 }
+
+// Solution 2. With memory.
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s == null || s.isEmpty() || wordDict.contains(s)) {
+            return true;
+        }
+        return wordBreakHelper(s, 0, wordDict, new HashMap<Integer, Boolean>());
+    }
+    private boolean wordBreakHelper(String s, int start, List<String> wordDict, Map<Integer, Boolean> memory) {
+        if (start == s.length()) {
+            return true;
+        }
+        if (memory.containsKey(start)) {
+            return memory.get(start);
+        }
+        for (int end = start; end < s.length(); ++end) {
+            if (wordDict.contains(s.substring(start, end+1))) {
+                boolean matched = wordBreakHelper(s, end+1, wordDict, memory);
+                memory.put(end+1, matched);
+                if (matched) {
+                    return true;
+                }
+            }
+        }
+        memory.put(start, false);
+        return false;
+    }
+}
