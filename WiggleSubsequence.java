69a18537a9a4335129923971705297f7a8a612f4
bellevueshaoyi <tiankong82@live.com>
2017-02-08 23:53:35 -0800

    Update WiggleSubsequence.java

--- WiggleSubsequence.java
+++ WiggleSubsequence.java
@@ -21,3 +21,27 @@ public class Solution {
         return Math.max(up, down);
     }
 }
+
+// 2. Greedy. O(N).  The logic is not trivial. Had many bugs. Need to consider: 
+// 1. [2,2,3,3,4,4]: return 3!
+// 2. [2,2,2,2,2]: return 1
+// 3. [2,2,3,3,4,3,3,4]: return 4
+
+public class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length<=1) {
+            return nums.length;
+        }
+        int len = 1;
+        // It's a good idea to use prediff instead of a boolean.
+        int prediff = 0;
+        for (int i=1; i<nums.length; ++i) {
+            int diff = nums[i] - nums[i-1];
+            if ((diff > 0 && prediff <= 0) || (diff < 0 && prediff >= 0)) {
+                ++len;
+                prediff = diff;
+            }
+        }
+        return len;
+    }
+}
