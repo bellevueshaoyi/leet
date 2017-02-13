068817a8526df2a9548917b8f72d022f0208fdcf
bellevueshaoyi <tiankong82@live.com>
2017-02-12 16:09:47 -0800

    Update MaximumSquare.java

--- MaximumSquare.java
+++ MaximumSquare.java
@@ -1,5 +1,28 @@
 // https://leetcode.com/problems/maximal-square/
-// Solution 1.
+// Solution 1.  Only keep one value. Check left, up and diagnoal.
+public class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] memory = new int[rows+1][cols+1];
+        int max = 0;
+        for (int i=0; i<rows; ++i) {
+            for (int j=0; j<cols; ++j) {
+                if (matrix[i][j] == '0') {
+                    continue;
+                }
+                memory[i+1][j+1] = Math.min(memory[i+1][j], Math.min(memory[i][j+1], memory[i][j])) + 1;
+                max = Math.max(max, memory[i+1][j+1]);
+            }
+        }
+        return max*max;
+    }
+}
+
+// Solution 2.
 public class Solution {
     public int maximalSquare(char[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
