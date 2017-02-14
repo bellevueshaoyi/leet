4b9d2b88ea3adcb967a0ced0cb2f381a8202687a
bellevueshaoyi <tiankong82@live.com>
2017-02-13 21:20:11 -0800

    Create BestTimeToSellStocksII.java

---
+++ BestTimeToSellStocksII.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/articles/best-time-buy-and-sell-stock-ii/.
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int max = 0, low, high;
+        int i=0;
+        // 这个很容易搞错!!当已经到了最后一个的时候要quit.
+        while (i+1 < prices.length) {
+            low = high = prices[i];
+            while (i+1 < prices.length && prices[i+1] >= prices[i]) {
+                high = prices[i+1];
+                ++i;
+            }
+            max += high-low;
+            while (i+1<prices.length && prices[i+1] < prices[i]) {
+                ++i;
+            }
+        }
+        return max;
+    }
+}
