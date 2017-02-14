5a1c35bb5eae1763f96306dee87417383063dcd8
bellevueshaoyi <tiankong82@live.com>
2017-02-13 21:23:07 -0800

    Update BestTimeToSellStocksII.java

--- BestTimeToSellStocksII.java
+++ BestTimeToSellStocksII.java
@@ -22,3 +22,19 @@ public class Solution {
         return max;
     }
 }
+
+// Solution 2.
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int max = 0;
+        for (int i=0; i+1<prices.length; ++i) {
+            if (prices[i+1] > prices[i]) {
+                max += prices[i+1]-prices[i];
+            }
+        }
+        return max;
+    }
+}
