167ce8f5e01d29544162e558b8db6ffd96de61e6
bellevueshaoyi <tiankong82@live.com>
2017-02-26 15:03:53 -0800

    Update JumpGame.java

--- JumpGame.java
+++ JumpGame.java
@@ -34,3 +34,34 @@ public class Solution {
         return index == nums.length;
     }
 }
+
+// 第二种解法, top down recursive with memorization
+enum Index {
+    UNKNOWN, CAN_REACH_END, CANNOT_REACH_END
+}
+public class Solution {
+    public boolean canJump(int[] nums) {
+        if (nums == null) {
+            return true;
+        }
+        Index[] memory = new Index[nums.length];
+        memory[nums.length-1] = Index.CAN_REACH_END;
+        return canJumpFrom(0, nums, memory);
+    }
+    boolean canJumpFrom(int start, int[] nums, Index[] memory) {
+        if (start >= nums.length) {
+            return true;
+        }
+        if (memory[start] != Index.UNKNOWN) {
+            return memory[start] == Index.CAN_REACH_END;
+        }
+        for (int i=start+1; i<nums.length && i <= start+nums[start];++i) {
+            if (canJumpFrom(i, nums, memory)) {
+                memory[start] = Index.CAN_REACH_END;
+                return true;
+            }
+        }
+        memory[start] = Index.CANNOT_REACH_END;
+        return false;
+    }
+}
